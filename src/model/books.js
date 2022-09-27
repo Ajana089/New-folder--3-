ed8ee7fed27910be50246ab8d38ee702d6dbd663
ajanaantony089@gmail.com <ajanaantony089@gmail.com>
2022-09-27 15:50:26 +0400

    added

--- src/model/books.js
+++ src/model/books.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
-const url = "mongodb+srv://ajana:WXEfL58SV4RttU1P@cluster0.skibkhl.mongodb.net/librarybookapplication?retryWrites=true&w=majority"
+//const url = "mongodb+srv://ajana:WXEfL58SV4RttU1P@cluster0.skibkhl.mongodb.net/librarybookapplication?retryWrites=true&w=majority"
+const url = "mongodb+srv://ajana:WXEfL58SV4RttU1P@cluster0.skibkhl.mongodb.net/librarybookapplication"
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }); 
 //mongoose.connect('mongodb://localhost:27017/librarybookapplication')
 const Schema=mongoose.Schema;
